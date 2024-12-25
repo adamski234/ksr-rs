01cb84553c5d7620649abeeb25ebd78f858717d7
adamski234 <adamski234@protonmail.com>
2024-12-25 19:24:59 +0100

    Implemented child-to-parent checkbox setting

--- src/main.rs
+++ src/main.rs
@@ -11,15 +11,27 @@ fn main() -> Result<(), Box<dyn Error>> {
 	ui.on_quantifier_root_toggled(move |tree_index| {
 		handle_checkbox_tree_root_toggle(tree_index, ui_weak.unwrap().get_quantifiers());
 	});
+	let ui_weak = ui.as_weak();
+	ui.on_quantifier_child_toggled(move |tree_index, entry_index| {
+		handle_checkbox_tree_child_toggle(tree_index, entry_index, ui_weak.unwrap().get_quantifiers());
+	});
 
 	let ui_weak = ui.as_weak();
 	ui.on_qualifier_root_toggled(move |tree_index| {
 		handle_checkbox_tree_root_toggle(tree_index, ui_weak.unwrap().get_qualifiers());
 	});
+	let ui_weak = ui.as_weak();
+	ui.on_qualifier_child_toggled(move |tree_index, entry_index| {
+		handle_checkbox_tree_child_toggle(tree_index, entry_index, ui_weak.unwrap().get_qualifiers());
+	});
 
 	let ui_weak = ui.as_weak();
 	ui.on_variable_root_toggled(move |tree_index| {
 		handle_checkbox_tree_root_toggle(tree_index, ui_weak.unwrap().get_variables());
+	});
+	let ui_weak = ui.as_weak();
+	ui.on_variable_child_toggled(move |tree_index, entry_index| {
+		handle_checkbox_tree_child_toggle(tree_index, entry_index, ui_weak.unwrap().get_variables());
 	});
 
 	ui.run()?;
@@ -30,12 +42,22 @@ fn main() -> Result<(), Box<dyn Error>> {
 fn handle_checkbox_tree_root_toggle(tree_index: i32, data: ModelRc<CheckBoxTreeData>) {
 	let tree_index = tree_index as usize;
 	assert!(tree_index < data.row_count());
-	let quantifier_tree = data.row_data(tree_index).unwrap();
-	let toggle_target = quantifier_tree.root.checked;
+	let checkbox_tree = data.row_data(tree_index).unwrap();
+	let toggle_target = checkbox_tree.root.checked;
 
-	for index in 0..quantifier_tree.children.row_count() {
-		let mut target = quantifier_tree.children.row_data(index).unwrap();
+	for child_index in 0..checkbox_tree.children.row_count() {
+		let mut target = checkbox_tree.children.row_data(child_index).unwrap();
 		target.checked = toggle_target;
-		quantifier_tree.children.set_row_data(index, target);
+		checkbox_tree.children.set_row_data(child_index, target);
 	}
+}
+
+// FIXME: This breaks if you manually toggle the root. See https://github.com/slint-ui/slint/discussions/7217
+fn handle_checkbox_tree_child_toggle(tree_index: i32, _child_index: i32, data: ModelRc<CheckBoxTreeData>) {
+	let tree_index = tree_index as usize;
+	assert!(tree_index < data.row_count());
+	let mut checkbox_tree = data.row_data(tree_index).unwrap();
+	let all_children_toggled = checkbox_tree.children.iter().all(|item| item.checked);
+	checkbox_tree.root.checked = all_children_toggled;
+	data.set_row_data(tree_index, checkbox_tree);
 }
