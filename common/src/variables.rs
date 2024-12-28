c5b2df06a3b96661622066519c9b0fa5103417ff
adamski234 <adamski234@protonmail.com>
2024-12-28 02:45:02 +0100

    Added saving variables to file, minor changes

--- common/src/variables.rs
+++ common/src/variables.rs
@@ -1,4 +1,4 @@
-use std::{fs::read_to_string, io::ErrorKind, path::Path};
+use std::{fs::{self, read_to_string}, io::ErrorKind, path::Path};
 
 #[derive(Debug, Clone, serde::Deserialize, serde::Serialize)]
 pub struct Label {
@@ -38,6 +38,49 @@ pub struct VariableFile {
 	pub quantifiers: Vec<LinguisticVariable>,
 }
 
+impl VariableFile {
+	pub fn parse_file(file: &Path) -> Result<Self, FileLoadError> {
+		match read_to_string(file) {
+			Ok(json_string) => {
+				match serde_json::from_str(&json_string) {
+					Ok(data) => return Ok(data),
+					Err(data) => {
+						match data.classify() {
+							serde_json::error::Category::Io => panic!("serde IO error from in-memory parsing: {:?}", data),
+							_ => return Err(FileLoadError::InvalidJSON),
+						}
+					}
+				}
+			}
+			Err(data) => {
+				match data.kind() {
+					std::io::ErrorKind::NotFound => return Err(FileLoadError::FileNotFound),
+					std::io::ErrorKind::PermissionDenied => return Err(FileLoadError::PermissionDenied),
+					other_error => return Err(FileLoadError::OtherError(other_error))
+				}
+			}
+		}
+	}
+	
+	pub fn save_file(&self, file: &Path) -> Result<(), FileSaveError> {
+		match fs::write(file, serde_json::to_string_pretty(self).unwrap()) {
+			Ok(_) => {
+				return Ok(());
+			}
+			Err(data) => {
+				match data.kind() {
+					ErrorKind::PermissionDenied => return Err(FileSaveError::PermissionDenied),
+					ErrorKind::ReadOnlyFilesystem => return Err(FileSaveError::ReadOnlyFilesystem),
+					ErrorKind::StorageFull => return Err(FileSaveError::StorageFull),
+					ErrorKind::QuotaExceeded => return Err(FileSaveError::QuotaExceeded),
+					ErrorKind::FileTooLarge => return Err(FileSaveError::FileTooLarge),
+					other => return Err(FileSaveError::OtherError(other)),
+				}
+			}
+		}
+	}
+}
+
 #[derive(Debug, Clone)]
 pub enum FileLoadError {
 	FileNotFound,
@@ -46,35 +89,12 @@ pub enum FileLoadError {
 	OtherError(ErrorKind)
 }
 
-pub fn parse_file(file: &Path) -> Result<VariableFile, FileLoadError> {
-	match read_to_string(file) {
-		Ok(json_string) => {
-			match serde_json::from_str(&json_string) {
-				Ok(data) => {
-					return Ok(data);
-				}
-				Err(data) => {
-					match data.classify() {
-						serde_json::error::Category::Io => panic!("serde IO error from in-memory parsing: {:?}", data),
-						_ => {
-							return Err(FileLoadError::InvalidJSON);
-						}
-					}
-				}
-			}
-		}
-		Err(data) => {
-			match data.kind() {
-				std::io::ErrorKind::NotFound => {
-					return Err(FileLoadError::FileNotFound);
-				},
-				std::io::ErrorKind::PermissionDenied => {
-					return Err(FileLoadError::PermissionDenied);
-				},
-				other_error => {
-					return Err(FileLoadError::OtherError(other_error));
-				},
-			}
-		}
-	}
+#[derive(Debug, Clone)]
+pub enum FileSaveError {
+	PermissionDenied,
+	ReadOnlyFilesystem,
+	StorageFull,
+	QuotaExceeded,
+	FileTooLarge,
+	OtherError(ErrorKind)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,30 @@
-use std::{cell::RefCell, env::current_dir, error::Error, rc::Rc};
+use std::{cell::RefCell, env::current_dir, error::Error, ops::Deref, rc::Rc};
 
-use common::variables::FileLoadError;
+use common::variables::{FileLoadError, FileSaveError, VariableFile};
 use rfd::MessageButtons;
 use slint::{Model, ModelRc};
 
 slint::include_modules!();
 
 fn file_load_error_to_message(err: FileLoadError) -> String {
+	use FileLoadError::*;
 	match err {
-		FileLoadError::FileNotFound => {
-			return String::from("The requested file could not be found.")
-		}
-		FileLoadError::InvalidJSON => {
-			return String::from("The requested file did not contain valid JSON.")
-		}
-		FileLoadError::PermissionDenied => {
-			return String::from("Permission denied when trying to access file.")
-		}
-		FileLoadError::OtherError(error_data) => {
-			return format!("Unknown error:\n {:#?}", error_data)
-		}
+		FileNotFound => return String::from("The requested file could not be found."),
+		InvalidJSON => return String::from("The requested file did not contain valid JSON."),
+		PermissionDenied => return String::from("Permission denied when trying to access file."),
+		OtherError(error_data) => return format!("Unknown error:\n {:#?}", error_data),
+	}
+}
+
+fn file_save_error_to_message(err: FileSaveError) -> String {
+	use FileSaveError::*;
+	match err {
+		PermissionDenied => return String::from("Permission denied when trying to write to file."),
+		ReadOnlyFilesystem => return String::from("The selected file is on a read-only filesystem."),
+		StorageFull => return String::from("Storage is full."),
+		QuotaExceeded => return String::from("Quota exceeded."),
+		FileTooLarge => return String::from("The file is too large."),
+		OtherError(error_data) => return format!("Unknown error:\n {:#?}", error_data),
 	}
 }
 
@@ -55,11 +60,12 @@ fn main() -> Result<(), Box<dyn Error>> {
 		handle_checkbox_tree_child_toggle(tree_index, entry_index, ui_weak.unwrap().get_variables());
 	});
 
+	let vars_clone = vars.clone();
 	let ui_weak = ui.as_weak();
 	ui.on_load_vars_pressed(move || {
 		let ui = ui_weak.unwrap();
 		ui.set_load_vars_open(true);
-		let vars = vars.clone();
+		let vars = vars_clone.clone();
 		slint::spawn_local(async move {
 			let picked_file = rfd::AsyncFileDialog::new()
 				.add_filter("JSON files", &["json"])
@@ -67,10 +73,11 @@ fn main() -> Result<(), Box<dyn Error>> {
 				.set_title("Choose JSON file with variables")
 				.pick_file().await;
 			if let Some(picked_file) = picked_file {
-				match common::variables::parse_file(picked_file.path()) {
+				match VariableFile::parse_file(picked_file.path()) {
 					Ok(data) => {
 						// Should not die since buttons are locked until processing ends
 						*vars.borrow_mut() = Some(data);
+						ui.set_variables_loaded(true);
 					}
 					Err(error) => {
 						rfd::AsyncMessageDialog::new()
@@ -82,6 +89,39 @@ fn main() -> Result<(), Box<dyn Error>> {
 				}
 			}
 			ui.set_load_vars_open(false);
+		}).unwrap();
+	});
+
+	let vars_clone = vars.clone();
+	let ui_weak = ui.as_weak();
+	ui.on_save_vars_pressed(move || {
+		let ui = ui_weak.unwrap();
+		ui.set_save_vars_open(true);
+		let vars = vars_clone.clone();
+		slint::spawn_local(async move {
+			if let Some(data) = &vars.borrow().deref() {
+				let picked_file = rfd::AsyncFileDialog::new()
+					.add_filter("JSON files", &["json"])
+					.set_directory(current_dir().unwrap())
+					.set_title("Choose JSON file with variables")
+					.save_file().await;
+				if let Some(picked_file) = picked_file {
+					if let Err(error) = data.save_file(picked_file.path()) {
+						rfd::AsyncMessageDialog::new()
+							.set_title("Variables not saved")
+							.set_description(file_save_error_to_message(error))
+							.set_buttons(MessageButtons::Ok)
+							.show().await;
+					}
+				}
+			} else {
+				rfd::AsyncMessageDialog::new()
+					.set_title("Variables not saved")
+					.set_description("There are no loaded variables")
+					.set_buttons(MessageButtons::Ok)
+					.show().await;
+			}
+			ui.set_save_vars_open(false);
 		}).unwrap();
 	});
 
